411e51abe761550014b929d7e02c8ec1db9c97ab
Thomas Matthias <Mindspyk@Thomass-MacBook-Pro.local>
2016-10-31 22:06:01 -0700

    Added .gitignore and commented out MIDICallback and MessageListener

--- MIDIBinz/Source/MainComponent.cpp
+++ MIDIBinz/Source/MainComponent.cpp
@@ -114,9 +114,9 @@ public:
 class MainContentComponent   : public AudioAppComponent, 
                                public Slider::Listener,
                                public ButtonListener,
-                               private Timer,
-                               private MidiInputCalback,
-                               private MessengerListener
+                               private Timer
+                               //private MidiInputCalback,
+                               //private MessengerListener
 {
 public:
     //==============================================================================
